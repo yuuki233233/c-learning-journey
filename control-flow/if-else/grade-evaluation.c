2592222b87682f550c340585c70a9a195f3e986c
Yuuki <yuuki233233@Outlook.com>
2025-09-02 23:10:20 +0800

    优化:增加了循环语句和退出

--- control-flow/if-else/grade-evaluation.c
+++ control-flow/if-else/grade-evaluation.c
@@ -1,39 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-int main()
-{
-	printf("请输入您的分数(0 ~ 100)之间,并查询评估:");
-	int score = 0;
-	if(scanf("%d", &score) != 1)
-	{
-		printf("错误:请输入有效数字!\n");
-		return 1;
-	}
 
-	if (score >= 90 && score <= 100)
+	int main()
 	{
-		printf("您的评估为优秀");
-	}
-	else if (score >= 80 && score < 90)
-	{
-		printf("您的评估为良好");
-	}
-	else if (score >= 70 && score < 80)
-	{
-		printf("您的评估为中等");
-	}
-	else if (score >= 60 && score < 70)
-	{
-		printf("您的评估为及格");
-	}
-	else if (score < 60)
-	{
-		printf("您的评估为不及格");
-	}
-	else
-	{
-		printf("您输入的分数无效");
-	}
+		do{
+		printf("请输入您的分数(0 ~ 100)之间,并查询评估,输入-1退出:");
+		int score = 0;
+		if (scanf("%d", &score) != 1)
+		{
+			printf("错误:请输入有效数字!\n");
+			return 1;
+		}
+
+		if (-1 == score) break;
+
+		if (score >= 90 && score <= 100)
+		{
+			printf("您的评估为优秀\n");
+		}
+		else if (score >= 80 && score < 90)
+		{
+			printf("您的评估为良好\n");
+		}
+		else if (score >= 70 && score < 80)
+		{
+			printf("您的评估为中等\n");
+		}
+		else if (score >= 60 && score < 70)
+		{
+			printf("您的评估为及格\n");
+		}
+		else if (score < 60)
+		{
+			printf("您的评估为不及格\n");
+		}
+		else
+		{
+			printf("您输入的分数无效\n");
+		}
+	}while(1);
 	return 0;
 }
