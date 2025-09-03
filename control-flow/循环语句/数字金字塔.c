e986be67696f4e74a0f01810b423341620709466
Yuuki <yuuki233233@Outlook.com>
2025-09-03 16:24:31 +0800

    feas:数字金字塔2025-9-3

--- control-flow/循环语句/数字金字塔.c
+++ control-flow/循环语句/数字金字塔.c
@@ -10,10 +10,10 @@ int main() {
 		for (int j = 1; j <= n - i; j++) {
 			printf(" ");
 		}
-		for (int k = 1; k <= i; k++) {
-			printf("%d", k);
+		for (int j = 1; j <= i; j++) {
+			printf("%d", j);
 		}
-		for (int k = i - 1; k >= 1; k--) {
+		for (int k = i - 1; k > 0; k--) {
 			printf("%d", k);
 		}
 		printf("\n");
