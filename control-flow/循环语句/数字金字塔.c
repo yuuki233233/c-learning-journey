27cf5d82bdf8609d90e38c97e5bb792d6b196a94
Yuuki <yuuki233233@Outlook.com>
2025-09-03 11:52:24 +0800

    feat:数字金字塔2025-9-3

---
+++ control-flow/循环语句/数字金字塔.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int n = 0;
+	printf("输入金字塔的层数: ");
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			printf(" ");
+		}
+		for (int k = 1; k <= i; k++) {
+			printf("%d", k);
+		}
+		for (int k = i - 1; k >= 1; k--) {
+			printf("%d", k);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
