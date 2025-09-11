5d971552d076ced1c8b83f0b385e901d474c44eb
Yuuki <yuuki233233@Outlook.com>
2025-09-11 18:20:22 +0800

    完成函数递归

---
+++ 函数学习/基础函数/函数递归.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int Fact(unsigned int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * Fact(n - 1);
+}
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int ret = Fact(n);
+	printf("%d\n", ret);
+
+	return 0;
+}
