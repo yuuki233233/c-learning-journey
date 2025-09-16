6f22a713c94605dbc78a357dab7a255b967c1cc5
Yuuki <yuuki233233@Outlook.com>
2025-09-16 18:20:52 +0800

    通过指针与函数来进行变量互换

---
+++ 指针/指针基础概念/指针变量互换.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//通过地址互换改变a与b的值
+void swap(int* x, int* y)
+{
+	int tamp = *x;
+	*x = *y;
+	*y = tamp;
+}
+
+int main()
+{
+	int a = 0, b = 0;
+	scanf("%d%d", &a, &b);
+	printf("交换前的值a = %d , b = %d\n", a, b);
+
+	swap(&a, &b);
+
+	printf("交换后的值a = %d , b = %d\n", a, b);
+
+	return 0;
+}
