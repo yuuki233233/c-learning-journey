0f77cd49b5ffe5e700c7d896eced0c64e105901f
Yuuki <yuuki233233@Outlook.com>
2025-09-20 17:59:13 +0800

    增加了交换元素的函数

--- 指针学习/高级指针/冒泡排序.c
+++ 指针学习/高级指针/冒泡排序.c
@@ -1,6 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+void find_arr(int* arr, int len);
+void swap(int* x, int* y);
+
+int main()
+{
+	int arr[] = { 1,3,5,7,9,2,4,6,8,10 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	find_arr(arr, len);
+	return 0;
+}
+
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 void find_arr(int* arr, int len)
 {
 	for (int i = 0; i < len; i++)
@@ -9,9 +27,7 @@ void find_arr(int* arr, int len)
 		{
 			if (*(arr + j) > *(arr + i))
 			{
-				int tamp = *(arr +j);
-				*(arr + j) = *(arr + i);
-				*(arr + i) = tamp;
+				swap((arr + j), (arr + i));
 			}
 		}
 	}
@@ -19,12 +35,4 @@ void find_arr(int* arr, int len)
 	{
 		printf("%d ", *(arr + i));
 	}
-}
-
-int main()
-{
-	int arr[] = { 1,3,5,7,9,2,4,6,8,10 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	find_arr(arr, len);
-	return 0;
 }
