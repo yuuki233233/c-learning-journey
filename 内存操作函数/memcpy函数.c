4a02c3406cab33c0facdcca69cc7cee567bb3117
Yuuki <yuuki233233@Outlook.com>
2025-10-05 14:46:36 +0800

    memcpy函数

---
+++ 内存操作函数/memcpy函数.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>//包含头文件
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	//内存足够大
+	int arr2[20] = { 0 };
+	//拷贝内存,最右边数字表示字节
+	memcpy(arr2, arr1, 40);
+	//打印数组arr2
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+
+	return 0;
+}
