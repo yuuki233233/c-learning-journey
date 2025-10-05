b83aae9ff668d5f8f10a64ce0ce2b351f57dbd2b
Yuuki <yuuki233233@Outlook.com>
2025-10-05 21:24:51 +0800

    代码优化

--- 内存操作函数/memcpy函数模拟实现.c
+++ 内存操作函数/memcpy函数模拟实现.c
@@ -27,7 +27,7 @@ int main()
 	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int arr2[20] = { 0 };
 
-	void* ret = my_memcpy(arr2, arr1, 40);//传入实参
+	void* ret = my_memcpy(arr2, arr1, 10*sizeof(int));//传入实参
 
 	//打印数组arr2
 	for (int i = 0; i < 20; i++)
