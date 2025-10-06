1d330156badd2ca9b8357ee0c2425dbed6fad141
Yuuki <yuuki233233@Outlook.com>
2025-10-06 08:41:05 +0800

    memcmp函数

---
+++ 内存操作函数/memcmp函数.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>//Í·ÎÄ¼þ
+
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8 };
+	int arr2[] = { 1,2,3,4,8,8,8,8 };
+	int num1 = memcmp(arr1, arr2, 4 * sizeof(int));
+	int num2 = memcmp(arr1, arr2, 5 * sizeof(int));
+
+	printf("%d\n", num1);
+	printf("%d", num2);
+
+	return 0;
+}
